3c4709ab772ed2c56e83047556576377fb7f5b1d
Egnate Noniashvili <103636296+egnointhewoods@users.noreply.github.com>
2022-07-18 17:16:18 +0400

    add 'about' section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,43 @@
 import "../Component-Styles/about.scss";
-import "../";
 
-import ScrollAnimation from "react-animate-on-scroll";
+import Fade from "react-reveal/Fade";
 
 export default function About() {
   return (
-    <div className="about fadeInDownBig">
-      <ScrollAnimation animateIn="fadeInDownBig">
-        <h1 className="h1">DUDEDUDE</h1>
-      </ScrollAnimation>
-    </div>
+    <Fade up>
+      <div className="about fadeIn">
+        <div className="about-header">
+          <h2 className="about-number">01.</h2>
+          <h2 className="about-heading">About Me</h2>
+        </div>
+        <div className="paragraphsWrapper">
+          <div className="paragraphs">
+            <p>
+              I am a self-taught front-end web developer based in Tbilisi,
+              Georgia. I've been developing Web Apps since 2021. I consider
+              myself as a well-organised person, problem solver and an
+              independent coder with high attention to detail.
+            </p>
+            <p>
+              I am a self-taught front-end web developer based in Tbilisi,
+              Georgia. I've been developing Web Apps since 2021. I consider
+              myself as a well-organised person, problem solver and an
+              independent coder with high attention to detail.
+            </p>
+            <p>
+              I am a self-taught front-end web developer based in Tbilisi,
+              Georgia. I've been developing Web Apps since 2021. I consider
+              myself as a well-organised person, problem solver and an
+              independent coder with high attention to detail.
+            </p>
+          </div>
+          <img
+            src="https://www.upload.ee/image/14173732/About_photo.PNG"
+            alt="profile"
+            className="profile"
+          />
+        </div>
+      </div>
+    </Fade>
   );
 }
--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -6,7 +6,9 @@ export default function Introduction() {
     <div className="introduction introductionFadeIn">
       <h1 className="x-small-heading">Hello, my name is</h1>
       <h2 className="big-heading">Egnate Noniashvili.</h2>
-      <h3 className="big-heading">I specialize in front end development.</h3>
+      <h3 className="big-heading">
+        I specialize in front end web development.
+      </h3>
       <p className="paragraph">
         I am a self-taught front-end web developer based in Tbilisi, Georgia.
         I've been developing Web Apps since 2021. I consider myself as a
