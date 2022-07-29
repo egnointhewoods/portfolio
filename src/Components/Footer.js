2b8608462275f97b453225ee23d8edf66759a489
Egnate Noniashvili <103636296+egnointhewoods@users.noreply.github.com>
2022-07-29 16:59:47 +0400

    Updated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,9 @@
 import "../Component-Styles/footer.css";
 
 export default function Footer() {
-  return <div className="footer">Built by Egnate Noniashvili</div>;
+  return (
+    <div className="footer">
+      Designed by Brittany Chiang & Built by Egnate Noniashvili
+    </div>
+  );
 }
