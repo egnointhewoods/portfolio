5386502a286828392a1b2cd86367e3bc5228d646
Egnate Noniashvili <103636296+egnointhewoods@users.noreply.github.com>
2022-08-13 02:04:15 +0400

    Add more functionality to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,8 @@ import { useContext } from "react";
 import { LanguageContext } from "../Context/LanguageContext";
 export default function Navbarr() {
   const { language, setLanguage } = useContext(LanguageContext);
-  console.log(language);
+  const [isLinksClicked, setIsLinksClicked] = useState(true); //listening to hamburger links click event
+
   const [scrollDir, setScrollDir] = useState(null);
   useEffect(() => {
     const threshold = 0;
@@ -34,104 +35,205 @@ export default function Navbarr() {
 
     return () => window.removeEventListener("scroll", onScroll);
   }, [scrollDir]);
-
-  return (
-    <>
-      <div
-        className={
-          !scrollDir
-            ? "nav navbarFadeIn"
-            : scrollDir === "down"
-            ? "nav scrolledNav"
-            : "nav scrolledNavUp"
-        }
-      >
-        <input type="checkbox" id="nav-check" />
-        <div className="nav-header">
-          <div className="nav-title">
-            <img
-              src="https://www.upload.ee/image/14307477/Egnate__4_.png"
-              alt="logo"
-              className="logo"
-            />
-            {language === "English" ? (
-              <button className="li">
-                Language <i className="down"></i>
-                <div className="dropdown">
-                  <button
-                    className="drop-link"
-                    onClick={() => setLanguage("Georgian")}
-                  >
-                    Georgian
-                  </button>
-                  <button
-                    className="drop-link"
-                    onClick={() => setLanguage("English")}
-                  >
-                    English
-                  </button>
-                </div>
-              </button>
-            ) : (
-              <li className="li">
-                ენა <i className="down"></i>
-                <div className="dropdown">
-                  <span
-                    className="drop-link"
-                    onClick={() => setLanguage("Georgian")}
-                  >
-                    ქართული
-                  </span>
-                  <span
-                    className="drop-link"
-                    onClick={() => setLanguage("English")}
-                  >
-                    ინგლისური
-                  </span>
-                </div>
-              </li>
-            )}
+  if (isLinksClicked) {
+    return (
+      <>
+        <div
+          className={
+            !scrollDir
+              ? "nav navbarFadeIn"
+              : scrollDir === "down"
+              ? "nav scrolledNav"
+              : "nav scrolledNavUp"
+          }
+        >
+          <input type="checkbox" id="nav-check" />
+          <div className="nav-header">
+            <div className="nav-title">
+              <img
+                src="https://www.upload.ee/image/14307477/Egnate__4_.png"
+                alt="logo"
+                className="logo"
+              />
+              {language === "English" ? (
+                <button className="li">
+                  Language <i className="down"></i>
+                  <div className="dropdown">
+                    <button
+                      className="drop-link"
+                      onClick={() => setLanguage("Georgian")}
+                    >
+                      Georgian
+                    </button>
+                    <button
+                      className="drop-link"
+                      onClick={() => setLanguage("English")}
+                    >
+                      English
+                    </button>
+                  </div>
+                </button>
+              ) : (
+                <li className="li">
+                  ენა <i className="down"></i>
+                  <div className="dropdown">
+                    <span
+                      className="drop-link"
+                      onClick={() => setLanguage("Georgian")}
+                    >
+                      ქართული
+                    </span>
+                    <span
+                      className="drop-link"
+                      onClick={() => setLanguage("English")}
+                    >
+                      ინგლისური
+                    </span>
+                  </div>
+                </li>
+              )}
+            </div>
+          </div>
+          {language === "English" ? (
+            <div className="nav-links rolledUpNavLinks">
+              <a href="#about">About</a>
+              <a href="#projects">Projects</a>
+              <a href="#contact">Contact</a>
+              <a
+                className="button"
+                id="resumeButton"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.canva.com/design/DAFBT0QqQdg/uJmGp1G31KbjuJ99XW2RHg/view?utm_content=DAFBT0QqQdg&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu"
+              >
+                Resume
+              </a>
+            </div>
+          ) : (
+            <div className="nav-links rolledUpNavLinks">
+              <a href="#about">ჩემ შესახებ</a>
+              <a href="#projects">პროექტები</a>
+              <a href="#contact">კონტაქტი</a>
+              <a
+                className="button"
+                id="resumeButton"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.canva.com/design/DAFBT0QqQdg/uJmGp1G31KbjuJ99XW2RHg/view?utm_content=DAFBT0QqQdg&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu"
+              >
+                რეზიუმე
+              </a>
+            </div>
+          )}
+          <div className="nav-btn" onClick={() => setIsLinksClicked(false)}>
+            <label htmlFor="nav-check">
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
           </div>
         </div>
-        {language === "English" ? (
-          <div className="nav-links">
-            <a href="#about">About</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact">Contact</a>
-            <a
-              className="button"
-              id="resumeButton"
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.canva.com/design/DAFBT0QqQdg/uJmGp1G31KbjuJ99XW2RHg/view?utm_content=DAFBT0QqQdg&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu"
-            >
-              Resume
-            </a>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div
+          className={
+            !scrollDir
+              ? "nav navbarFadeIn"
+              : scrollDir === "down"
+              ? "nav scrolledNav"
+              : "nav scrolledNavUp"
+          }
+        >
+          <input type="checkbox" id="nav-check" />
+          <div className="nav-header">
+            <div className="nav-title">
+              <img
+                src="https://www.upload.ee/image/14307477/Egnate__4_.png"
+                alt="logo"
+                className="logo"
+              />
+              {language === "English" ? (
+                <button className="li">
+                  Language <i className="down"></i>
+                  <div className="dropdown">
+                    <button
+                      className="drop-link"
+                      onClick={() => setLanguage("Georgian")}
+                    >
+                      Georgian
+                    </button>
+                    <button
+                      className="drop-link"
+                      onClick={() => setLanguage("English")}
+                    >
+                      English
+                    </button>
+                  </div>
+                </button>
+              ) : (
+                <li className="li">
+                  ენა <i className="down"></i>
+                  <div className="dropdown">
+                    <span
+                      className="drop-link"
+                      onClick={() => setLanguage("Georgian")}
+                    >
+                      ქართული
+                    </span>
+                    <span
+                      className="drop-link"
+                      onClick={() => setLanguage("English")}
+                    >
+                      ინგლისური
+                    </span>
+                  </div>
+                </li>
+              )}
+            </div>
           </div>
-        ) : (
-          <div className="nav-links">
-            <a href="#about">ჩემ შესახებ</a>
-            <a href="#projects">პროექტები</a>
-            <a href="#contact">კონტაქტი</a>
-            <a
-              className="button"
-              id="resumeButton"
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.canva.com/design/DAFBT0QqQdg/uJmGp1G31KbjuJ99XW2RHg/view?utm_content=DAFBT0QqQdg&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu"
-            >
-              რეზიუმე
-            </a>
+          {language === "English" ? (
+            <div className="nav-links" onClick={() => setIsLinksClicked(true)}>
+              <a href="#about">About</a>
+              <a href="#projects">Projects</a>
+              <a href="#contact">Contact</a>
+              <a
+                className="button"
+                id="resumeButton"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.canva.com/design/DAFBT0QqQdg/uJmGp1G31KbjuJ99XW2RHg/view?utm_content=DAFBT0QqQdg&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu"
+              >
+                Resume
+              </a>
+            </div>
+          ) : (
+            <div className="nav-links" onClick={() => setIsLinksClicked(true)}>
+              <a href="#about">ჩემ შესახებ</a>
+              <a href="#projects">პროექტები</a>
+              <a href="#contact">კონტაქტი</a>
+              <a
+                className="button"
+                id="resumeButton"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.canva.com/design/DAFBT0QqQdg/uJmGp1G31KbjuJ99XW2RHg/view?utm_content=DAFBT0QqQdg&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu"
+              >
+                რეზიუმე
+              </a>
+            </div>
+          )}
+          <div className="nav-btn" onClick={() => setIsLinksClicked(false)}>
+            <label htmlFor="nav-check">
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
           </div>
-        )}
-        <div className="nav-btn">
-          <label htmlFor="nav-check">
-            <span></span>
-            <span></span>
-            <span></span>
-          </label>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
